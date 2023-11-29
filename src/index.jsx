a6bcdcdc7b4fe9963257e3d404f5b6b1d7cd5ac7
juanpaup07 <juanpaup07@gmail.com>
2023-11-29 18:16:50 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const express = require('express');
+const app = express();
 
+const PORT = process.env.PORT || 3000;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
@@ -11,4 +14,6 @@ root.render(
   </React.StrictMode>
   
 );
-
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
